7b82bc76423e51cf37d4b947f0633ad539edcd80
unknown <Administrator@PWServer22.bentley.com>
2025-05-16 15:49:14 +0300

    this app calcs compounding and displays it

--- PythonIntroToCS/chapter_4/futval_graph2_logicalGrid.py
+++ PythonIntroToCS/chapter_4/futval_graph2_logicalGrid.py
@@ -1,26 +1,53 @@
 from graphics import * 
 
 def main():
-    # A default window size 320x240
-    main_win=GraphWin("Investment Growth Chart",320,240)
-    main_win.setCoords(-1,-1,10,10000)
     
-    principal=3000
-    Rectangle(Point(0,0),Point(1,principal)).draw(main_win).setFill("green") 
-    Text(Point(0,principal),principal).draw(main_win).setFill("white")
-    apr=.05
-    principal=principal*(1+apr)
-    principal2=principal*(1+apr)
-    print(principal)
-    Rectangle(Point(1,0),Point(2,principal)).draw(main_win).setFill("green")
-    Rectangle(Point(2,0),Point(3,principal2)).draw(main_win).setFill("green")
+    # Introduction
+    print("This program plots the growth of a 10-year investment.")
     
-    Text(Point(1,principal/2),principal).draw(main_win).setFill("white")
-    Text(Point(2,principal2/4),principal2).draw(main_win).setFill("white")
-    while True:
+    # Get principal and interest rate
+    principal = float(input("Enter the init ial principal: "))
+    apr = float(input("Enter the annualized interest rate: "))
 
-        key=main_win.getKey()
-        if key=='q':
-            break
+    # Create a graphics window size of 320x240, 640x480
+    main_win=GraphWin("Investment Growth Chart",640,480)
+    main_win.setCoords(-1.75,-250,11.5,10500)
+    
+    
+    # Create labels on left edge
+    label_0=Text(Point(-1,0),"0.0k")
+    label_0.draw(main_win)
+    label_1=Text(Point(-1,2500),"2.5k")
+    label_1.draw(main_win)
+    label_2=Text(Point(-1,5000),"5.0k")
+    label_2.draw(main_win)
+    label_3=Text(Point(-1,7500),"7.5k")
+    label_3.draw(main_win)
+    label_4=Text(Point(-1,10000),"10.0k")
+    label_4.draw(main_win)
 
-# draw_one_bar()
+    # Draw bar for initial principal 
+    init_bar=Rectangle(Point(0,0),Point(1,principal))
+    init_bar.setFill("blue")
+    init_bar.setWidth(2) # outline of rectabgle's shape
+    init_bar.draw(main_win)
+
+    # Draw a bar f or each subsequent year
+    for year in range(1,11):
+        principal=principal*(1+apr)
+
+        bar=Rectangle(Point(year,0),Point(year+1,principal))
+        bar.setWidth(2)
+        bar.setFill("green")
+        bar.draw(main_win)
+
+    input("Press any key to quit.")
+    main_win.close()
+
+    # while True:
+
+    #     key=main_win.getKey()
+    #     if key=='q':
+    #         break
+
+main()
