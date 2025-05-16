e3c2c6d0734f6000e33fcb06878591acb65127e9
Andzej Krupoves <32535723+AndzejK@users.noreply.github.com>
2025-05-16 07:53:49 +0300

    Update futval_graph2_logicalGrid.py

--- PythonIntroToCS/chapter_4/futval_graph2_logicalGrid.py
+++ PythonIntroToCS/chapter_4/futval_graph2_logicalGrid.py
@@ -2,22 +2,25 @@ from graphics import *
 
 def main():
     # A default window size 320x240
-# main_win=GraphWin("Investment Growth Chart",320,240)
-
-# The value of the investment 10 years into the future.
-    principal=float(input("Enter your principal: "))
-    apr=float(input("Enter the annual percentage rate: ")) # The annual percentage rate expressed as a decimal number
-
-    for year in range(1,11):
-        print(f"Year {year} - {round(principal,2)} $")
-        principal=principal*(1+apr)
-
-def draw_one_bar():
-    # A default window size 320x240
     main_win=GraphWin("Investment Growth Chart",320,240)
+    main_win.setCoords(-1,-1,10,10000)
+    
     principal=3000
+    Rectangle(Point(0,0),Point(1,principal)).draw(main_win).setFill("green") 
+    Text(Point(0,principal),principal).draw(main_win).setFill("white")
     apr=.05
     principal=principal*(1+apr)
+    principal2=principal*(1+apr)
     print(principal)
+    Rectangle(Point(1,0),Point(2,principal)).draw(main_win).setFill("green")
+    Rectangle(Point(2,0),Point(3,principal2)).draw(main_win).setFill("green")
+    
+    Text(Point(1,principal/2),principal).draw(main_win).setFill("white")
+    Text(Point(2,principal2/4),principal2).draw(main_win).setFill("white")
+    while True:
+
+        key=main_win.getKey()
+        if key=='q':
+            break
 
 # draw_one_bar()
